1f7b8f2bc46b82bc1469b66718d1936b154b5aed
Maxym Shalennyy <max.sv01@gmail.com>
2023-09-24 14:57:10 +0200

    feat(graphql-client): add client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,27 @@
-export default function Home() {
+
+import { gql } from "@apollo/client";
+import {getClient} from '@/client'
+import { Lesson } from "@/types/payload-types";
+
+const query = gql`query Lessons {
+  Lessons {
+    docs {
+      lessonName
+    }
+  }
+}`;
+
+interface ILessons{
+  Lessons: {
+    docs: Lesson[]
+  }
+}
+
+export default async function Home() {
+  const { data } = await getClient().query<ILessons>({ query });
+
+  console.log('data', data.Lessons.docs);
+  
   return (
     <main className="flex-[1_0_auto]">
         test
